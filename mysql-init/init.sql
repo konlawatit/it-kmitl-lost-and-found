a36a9deb38e8baece1e78638de68a15f9cc4ca73
bb0b <konlawatit@gmail.com>
2021-03-29 21:47:18 +0700

    fixed error init sql

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -34,7 +34,7 @@ CREATE TABLE USER (
 
 CREATE TABLE STUDENT (
     student_id char(8) not null unique,
-    branch enum('IT', 'DSBA', 'BIT')
+    branch enum('IT', 'DSBA', 'BIT'),
     `year` int(1),
     USER_user_id int(10),
     FOREIGN KEY (USER_user_id)
@@ -60,6 +60,7 @@ CREATE TABLE INFO_POST (
     USER_user_id1 int(10),
     USER_user_id2 int(10),
     complete_time datetime,
+    picture text,
     FOREIGN KEY (USER_user_id1)
     REFERENCES USER (`user_id`),
     FOREIGN KEY (USER_user_id2)
@@ -93,8 +94,8 @@ CREATE TABLE INFO_COMMENT_COMMENT_IMAGE (
 );
 
 CREATE TABLE INFO_CHAT (
-    chat_id int(10) primary key auto_increment,
-    chat_no int(10) primary key not null unique auto_increment,
+    chat_id int(10) primary key,
+    chat_no int(10) not null unique auto_increment,
     `message` varchar(255),
     chat_time datetime,
     USER_user_id1 int(10),
@@ -115,7 +116,7 @@ CREATE TABLE INFO_CHAT_CHAT_IMAGE (
 
 CREATE TABLE CATAGORY_ITEM (
     item_id int(10) primary key auto_increment,
-    `name` int(10) varchar(255),
+    `name` varchar(255),
     USER_user_id int(10),
     FOREIGN KEY (USER_user_id)
     REFERENCES USER (`user_id`)
@@ -131,7 +132,7 @@ CREATE TABLE CATAGORY_ITEM_INFO_POST (
 );
 
 INSERT INTO INFO_POST(user_id, topic, post_desc, post_time, picture) VALUES('62070007', 'Tam ha nong Eng teon pom!!!', 'Hi!', '2021-03-28 10:10:10', 'https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/166327698_776500569906547_6614718348313859894_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=ae9488&_nc_eui2=AeElIe43co5I27xD-vlilj6GJqXI3E_8jPUmpcjcT_yM9fj6xVIaGkaXpsluV2GPMaHz-LFR_bt5zxXMUzdPpENO&_nc_ohc=pj_yFhtqvIUAX-PrYeG&_nc_ht=scontent.fbkk7-2.fna&oh=7b8f83b8d71e07466221b7b3285a553f&oe=6084B37D');
-INSERT INTO INFO_POST(user_id, topic, post_desc, post_time, picture) VALUES('62070007', 'Tam ha nong Eng teon pom!!!', 'Hi!', '2021-03-28 10:10:10', 'https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.15752-9/165827485_159358352623499_4855381441308277690_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=ae9488&_nc_eui2=AeG5GP9dDHp9F-R8uGmTE3ZlWCQUuLoXpXFYJBS4uhelcfpk-a1vm7ebx6Gw5bLnZSpzj4NBC6pemNtcTacvlmKo&_nc_ohc=KVmzka4Ic28AX9cg_Dl&_nc_ht=scontent.fbkk7-3.fna&oh=3813bc1f40c096bb3a8e01c615410481&oe=6088066E');
+INSERT INTO INFO_POST(user_id, topic, post_desc, post_time, picture) VALUES('62070007', 'Tam ha nong Eng teon pom!!!', 'Hi! ทดสอบ', '2021-03-28 10:10:10', 'https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.15752-9/165827485_159358352623499_4855381441308277690_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=ae9488&_nc_eui2=AeG5GP9dDHp9F-R8uGmTE3ZlWCQUuLoXpXFYJBS4uhelcfpk-a1vm7ebx6Gw5bLnZSpzj4NBC6pemNtcTacvlmKo&_nc_ohc=KVmzka4Ic28AX9cg_Dl&_nc_ht=scontent.fbkk7-3.fna&oh=3813bc1f40c096bb3a8e01c615410481&oe=6088066E');
 INSERT INTO INFO_POST(user_id, topic, post_desc, post_time, picture) VALUES('62070007', 'Tam ha nong Eng teon pom!!!', 'Hi!', '2021-03-28 10:10:10'
 , 'https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/165280283_859141274643190_8489336105055123969_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=ae9488&_nc_eui2=AeE0kaaNU3N1o_R0WeIT4dcI1x7pe0_wkXbXHul7T_CRdsMD99Itad3BoWYdDIeqXd3OIcXZPWxscjm-Zd8h_ihF&_nc_ohc=eN-r7TWbsfUAX8xmaj6&_nc_ht=scontent.fbkk7-2.fna&oh=f1ce50f06f3f45d70282473db571cb3e&oe=6085AD5C');
 INSERT INTO INFO_POST(user_id, topic, post_desc, post_time, picture) VALUES('62070007', 'Tam ha nong Eng teon pom!!!', 'Hi!', '2021-03-28 10:10:10', 'https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/165533935_896196034492239_3141671990484018006_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=ae9488&_nc_eui2=AeGINckz0sdZe5eB_emjvg3yyJKQ7EJ8UH7IkpDsQnxQfnmItyNEsECTN893PS6TKg0H8v5w5FeiN2RWMaEPtvbC&_nc_ohc=BaVIGS_UZZoAX_QNfxW&_nc_ht=scontent.fbkk7-2.fna&oh=85cc39502e7d669b182099ef9bf5fc80&oe=6085C758');
