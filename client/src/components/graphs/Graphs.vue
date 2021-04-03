2b5a9ab22e6040b8bbb545565852f6bb3c0d1bfb
TeerapatEarth <teerapat.boonchuaylaew@gmail.com>
2021-04-03 16:23:24 +0700

    Add graphs

---
+++ client/src/components/graphs/Graphs.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="columns">
+    <div class="column is-12">
+      <chart :type="'bar'" :data="data" :options="options"></chart>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from "vue-bulma-chartjs";
+export default {
+  components: {
+    Chart,
+  },
+  name: "graphs",
+  data() {
+    return {
+      data: {
+        labels: [
+          "62070096",
+          "62070007",
+          "62070027",
+          "62070126",
+          "62070001",
+          "62070011",
+          "62070034",
+          "62070078",
+          "62070077",
+          "62070123",
+        ],
+        datasets: [
+          {
+            label: "Score",
+            data: [80, 79, 68, 64, 63, 42, 40, 30, 26, 19],
+            backgroundColor: [
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057",
+                "#0f3057"
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- client/src/views/Leaderboard.vue
+++ client/src/views/Leaderboard.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
     <Navbarhome></Navbarhome>
+    <div class="columns mt-6">
+      <div class="column is-2"></div>
+      <div class="column is-8">
+        <Graphs></Graphs>
+      </div>
+      <div class="column is-2"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbarhome from "../components/navbar/Navbarhome";
+import Graphs from "../components/graphs/Graphs"
 export default {
   name: "CreatePost",
   components: {
     Navbarhome,
+    Graphs
   },
 };
 </script>
