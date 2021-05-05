3ac981352443e89d359e9b6a4ff42e0dd997a838
bb0b <konlawatit@gmail.com>
2021-05-05 23:43:24 +0700

    fix noti chat

--- client/src/components/IconChat.vue
+++ client/src/components/IconChat.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <div id="mybutton">
-    <v-badge avatar overlap :value="notifications">
-      <template v-slot:badge >
-              <v-avatar class="has-background-danger">
-                {{notifications}}
-              </v-avatar>
-      </template>
-      <button class="feedback" @click="redirect('chatroom')"><i class="fas fa-comments"></i></button>
-    </v-badge>
+      <v-badge avatar overlap :value="notifications">
+        <template v-slot:badge>
+          <v-avatar class="has-background-danger">
+            {{ notifications }}
+          </v-avatar>
+        </template>
+        <button class="feedback" @click="redirect('chatroom')">
+          <i class="fas fa-comments"></i>
+        </button>
+      </v-badge>
     </div>
   </div>
 </template>
@@ -17,80 +19,79 @@
 import { io } from "socket.io-client";
 const socket = io("http://localhost:8888");
 import { mapGetters } from "vuex";
-import  ChatService  from "../service/ChatService"
+import ChatService from "../service/ChatService";
 //import ChatService from "../service/ChatService"
 export default {
-  name: "IconChat", 
+  name: "IconChat",
   data() {
-    return {
-    };
+    return {};
   },
-  methods:{
+  methods: {
     redirect(path) {
       console.log("redirect to : ", path);
       this.$router.push(`/${path}`);
     },
   },
-   computed: {
+  computed: {
     ...mapGetters("conversation", ["getRooms"]),
     ...mapGetters("auth", ["getId"]),
-     notifications() {
+    notifications() {
       return this.getRooms.reduce((total, currentValue) => {
-      return total + currentValue.notification
-    }, 0)
-    }
+        return total + currentValue.notification;
+      }, 0);
+    },
   },
   async created() {
-    if (this.getId) { //ให้แสดงผลเมื่อกดมาจากปุ่ม chat
-        await ChatService.getRooms(this.$store.getters["auth/getId"]).then(
-          async (rooms) => {
-            console.log('list name', rooms)
-            this.$store.dispatch("conversation/setRooms", rooms);
-          }
-        );
-      }
+    if (this.getId) {
+      //ให้แสดงผลเมื่อกดมาจากปุ่ม chat
+      await ChatService.getRooms(this.$store.getters["auth/getId"]).then(
+        async (rooms) => {
+          console.log("list name", rooms);
+          this.$store.dispatch("conversation/setRooms", rooms);
+        }
+      );
+    }
     // let room = await ChatService.getRooms(this.$store.getters['auth/getId'])
     // console.log('fethc room', room)
     // this.notifications = this.getRooms.reduce((total, currentValue) => {
     //   return total + currentValue.notification
     // }, 0)
     // //this.notifications = room.reduce
-      socket.disconnect()
-      socket.connect()
-      socket.on("noti_chat", async (sender_id, con_id, receiver_id) => {
-        console.log('มีคนส่งมาาาาาา')
-        if (this.getId == receiver_id) {
-          //if (this.$router.app.$route.fullPath == '/home') {
-          //\if (this.getSelectRoom.user_id != sender_id) {
-            if (this.getRooms) {
-              await ChatService.getRooms(this.$store.getters["auth/getId"]).then(
-          async (rooms) => {
-            console.log('list name', rooms)
-            this.$store.dispatch("conversation/setRooms", rooms);
-          }
-        );
+    socket.disconnect();
+    socket.connect();
+    socket.on("noti_chat", async (sender_id, con_id, receiver_id) => {
+      console.log("มีคนส่งมาาาาาา");
+      if (this.getId == receiver_id) {
+        //if (this.$router.app.$route.fullPath == '/home') {
+        //\if (this.getSelectRoom.user_id != sender_id) {
+        if (!this.getRooms) {
+          await ChatService.getRooms(this.$store.getters["auth/getId"]).then(
+            async (rooms) => {
+              console.log("list name", rooms);
+              this.$store.dispatch("conversation/setRooms", rooms);
             }
-            this.getRooms.map(room => {
-              if (room.con_id == con_id) {
-                room['notification'] += 1
-              }
-              return room
-            })
-          //}
-             //this.$store.dispatch("conversation/seteRooms", setRoom)
-         }
-      })
+          );
+        }
+        this.getRooms.map((room) => {
+          console.log(room.notification)
+          if (room.con_id == con_id) {
+            room["notification"] += 1;
+          }
+          return room;
+        });
+        //}
+        //this.$store.dispatch("conversation/seteRooms", setRoom)
+      }
+    });
   },
-  async mounted () {
-    
-  }
+  async mounted() {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .feedback {
-  background-color : #31B0D5;
+  background-color: #31b0d5;
   color: white;
   padding: 0.2rem;
   font-size: 3rem;
@@ -105,8 +106,8 @@ export default {
   right: 2rem;
 }
 
-.feedback:hover{
-    background-color: #0d556b;
-    transition: 0.3s;
+.feedback:hover {
+  background-color: #0d556b;
+  transition: 0.3s;
 }
 </style>
--- client/src/components/chat/Listname.vue
+++ client/src/components/chat/Listname.vue
@@ -8,22 +8,21 @@
       }"
       v-for="(room, index) in getRooms"
       :key="room.con_id"
-      
     >
       <div class="columns p-3" @click="selectRoom(index)">
         <div class="column is-3">
           <v-badge avatar bordered overlap :value="room.notification">
-            <template v-slot:badge >
+            <template v-slot:badge>
               <v-avatar class="has-background-danger">
-                {{room.notification}}
+                {{ room.notification }}
               </v-avatar>
             </template>
-            <v-avatar  color="primary" size="56">
+            <v-avatar color="primary" size="56">
               <img :src="room.image" />
             </v-avatar>
           </v-badge>
         </div>
-        
+
         <div class="column is-9">
           <div class="mt-4 is-size-6">
             {{ room.user_name }}
@@ -63,12 +62,12 @@ export default {
     async selectRoom(index) {
       //let user_id = this.getRooms[index].user_id_1 == this.getId ? this.getRooms[index].user_id_2 : this.getRooms[index].user_id_1
       let user_id = this.getRooms[index].user_id;
-      let con_id = this.getRooms[index].con_id
+      let con_id = this.getRooms[index].con_id;
 
       this.$store.dispatch("conversation/setSelectRoom", {
         user_id: user_id,
         user_name: this.getRooms[index].user_name,
-        con_id: con_id
+        con_id: con_id,
       });
 
       await ChatService.getMessages({
@@ -78,52 +77,51 @@ export default {
         this.$store.dispatch("conversation/setMessages", data);
       });
 
-      await ChatService.clearNoti(this.getId, this.getRooms[index]['con_id']).then(() => {
-        console.log('clear noti')
-        this.getRooms[index]['notification'] = 0 //reset noti ให้เป็ร 0 เมื่อกด
-      })
-
+      await ChatService.clearNoti(
+        this.getId,
+        this.getRooms[index]["con_id"]
+      ).then(() => {
+        console.log("clear noti");
+        this.getRooms[index]["notification"] = 0; //reset noti ให้เป็ร 0 เมื่อกด
+      });
 
       let myDiv = window.document.getElementById("chatbox"); //ส่งให้ข้อความเลื่อลงด้านล่างของ componetn Chatbox
       myDiv.scrollTop = myDiv.scrollHeight;
     },
   },
   async mounted() {
-    
     if (this.getId) {
-
-    
       socket.on("noti_chat", async (sender_id, con_id) => {
-        if (this.$router.app.$route.fullPath == '/chatroom') {
-        if (this.getId != sender_id) { //ไม่เอาแชทของตัวเอง
-          if (this.getSelectRoom.user_id != sender_id) { //จะขึ้น noti กับ chat ที่ไม่ได้เลือก
-            this.getRooms.map(room => {
-              if (room.con_id == con_id) {
-                room['notification'] += 1
-              }
-              return room
-            })
-            //this.$store.dispatch("conversation/seteRooms", setRoom)
-          console.log("noto_chat", sender_id);
+        if (this.$router.app.$route.fullPath == "/chatroom") {
+          if (this.getId != sender_id) {
+            //ไม่เอาแชทของตัวเอง
+            if (this.getSelectRoom.user_id != sender_id) {
+              //จะขึ้น noti กับ chat ที่ไม่ได้เลือก
+              this.getRooms.map((room) => {
+                if (room.con_id == con_id) {
+                  room["notification"] += 1;
+                }
+                return room;
+              });
+              //this.$store.dispatch("conversation/seteRooms", setRoom)
+              console.log("noto_chat", sender_id);
+            }
           }
         }
-        }
-        
       });
-      }
-  }
-  ,
-    async created() {
-      
-      if (this.getId) { //ให้แสดงผลเมื่อกดมาจากปุ่ม chat
-        await ChatService.getRooms(this.$store.getters["auth/getId"]).then(
-          async (rooms) => {
-            console.log('list name', rooms)
-            this.$store.dispatch("conversation/setRooms", rooms);
-          }
-        );
-      }
-    },
+    }
+  },
+  async created() {
+    if (this.getId) {
+      //ให้แสดงผลเมื่อกดมาจากปุ่ม chat
+      await ChatService.getRooms(this.$store.getters["auth/getId"]).then(
+        async (rooms) => {
+          console.log("list name", rooms);
+          this.$store.dispatch("conversation/setRooms", rooms);
+        }
+      );
+    }
+  },
   computed: {
     ...mapGetters("conversation", ["getRooms", "getSelectRoom"]),
     ...mapGetters("auth", ["getId"]),
