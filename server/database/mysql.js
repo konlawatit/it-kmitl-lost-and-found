ed67744d47f52960b38ee611e9f05b7cd7e4a270
bb0b <59884776+konlawatit@users.noreply.github.com>
2021-11-20 23:31:58 +0700

    Update mysql.js

--- server/database/mysql.js
+++ server/database/mysql.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({ // config ค่าการเชื่อมต่อฐานข้อมูล
    host: 'db', //ใช้ docker ให้เปลี่ยนเป็น db ปกติ 127.0.0.1
    user: 'root',
-   password: '123456',
+   // password: '123456',
    // port: '3999', //ใช้ docker ให้ปิดตรงนี้
    database: `it_lost_and_found`,
    waitForConnections: true,
